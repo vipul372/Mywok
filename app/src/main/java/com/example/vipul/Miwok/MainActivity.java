bedbfdbd1669bfd8a28fe85ce27c4c342fa94809
vipul372 <vipulpanchal372@gmail.com>
2018-06-26 22:44:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vipul/Miwok/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.vipul.Miwok;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.vipul.Miwok.R.layout.activity_main);
+
+        TextView numbers = findViewById(com.example.vipul.Miwok.R.id.numbers);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+                Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+            }
+        });
+        TextView family = findViewById(com.example.vipul.Miwok.R.id.family);
+        family.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+                Toast.makeText(view.getContext(), "Open the list of family", Toast.LENGTH_SHORT).show();
+            }
+        });
+        TextView colors = findViewById(com.example.vipul.Miwok.R.id.colors);
+        colors.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this,ColorsActivity.class);
+                startActivity(colorsIntent);
+                Toast.makeText(view.getContext(), "Open the list of colors", Toast.LENGTH_SHORT).show();
+            }
+        });
+        TextView phrases = findViewById(com.example.vipul.Miwok.R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+                Toast.makeText(view.getContext(), "Open the list of phrases", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/vipul/Miwok/Word.java
@@ -0,0 +1,19 @@
+package com.example.vipul.Miwok;
+
+public class Word {
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
