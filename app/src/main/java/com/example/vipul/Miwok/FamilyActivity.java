8ce79a0236d81b22c7c610ba95534b2f1fbfafa7
vipul372 <vipulpanchal372@gmail.com>
2018-06-30 02:16:21 +0530

    multiple screen app example(without fragments) @Udacity

--- app/src/main/java/com/example/vipul/Miwok/FamilyActivity.java
+++ app/src/main/java/com/example/vipul/Miwok/FamilyActivity.java
@@ -13,6 +13,14 @@ public class FamilyActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
 
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,8 +64,27 @@ public class FamilyActivity extends AppCompatActivity {
                 Word word = words.get(position);
                 mediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceId());
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(mCompletionListener);
 
             }
         });
     }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
+    }
+
 }
--- app/src/main/java/com/example/vipul/Miwok/NumbersActivity.java
+++ app/src/main/java/com/example/vipul/Miwok/NumbersActivity.java
@@ -14,6 +14,14 @@ public class NumbersActivity extends AppCompatActivity {
 
 
     private MediaPlayer mediaPlayer;
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +60,28 @@ public class NumbersActivity extends AppCompatActivity {
                 Word word = words.get(position);
                 mediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
                 mediaPlayer.start();
-
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 
 }
